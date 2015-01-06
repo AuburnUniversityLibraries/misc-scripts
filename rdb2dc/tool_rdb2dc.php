cb1ed6f7d108f555bc9e2ba7644f95ba8c966e33
Clint Bellanger <clintbellanger@gmail.com>
2015-01-06 15:50:55 -0600

    Handle various XML encoding issues Also some restructure for legibility

--- rdb2dc/tool_rdb2dc.php
+++ rdb2dc/tool_rdb2dc.php
@@ -9,14 +9,24 @@ include_once('inc_db.php');
 // CONFIGURATION
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-define("DEBUG_MODE", true);
 
+// verbose debug mode outputs additional info messages
+define("VERBOSE", false);
+
+// test mode does not move the PDFs
+define("TEST_MODE", true);
+
+// TODO: change this to a lookup of the length of the largest pub_id?
+define("FOLDER_DIGITS", 4);
  
 class RDB2DC {
 
   private $db;
   private $outfile;
 
+  function escape_filename($filename) {
+    return str_replace(array(' ', '(', ')'), array('\ ', '\(', '\)'), $filename);
+  }
   
   /****************************************************************************/
   // General Database Functions
@@ -37,35 +47,48 @@ class RDB2DC {
   }
   
   /**
-   * query details for a specific publication
-   * then call a function to perform output/formatting
+   *
    */
-  function query_pub_details($pub_id, $sql, $format_function) {
+  function get_recordset($sql, $uid = NULL) {
+
+    $resultset = array();
+
     try {
       $stmt = $this->db->prepare($sql);
-      $stmt->bindParam(1, $pub_id);
+
+      if (!is_null($uid)) {
+        $stmt->bindParam(1, $uid);
+      }
 
       if ($stmt->execute()) {
         while ($row = $stmt->fetch()) {          
-          call_user_func(array($this, $format_function), $row);
+          $resultset[] = $row;
         }
       }      
     }
     catch (PDOException $pe) {
       echo "PDOException error: " . $pe->getMessage();
-    }    
+    }
+
+    return $resultset;
   }  
 
   /****************************************************************************/
   // General File Functions
 
+  function folder_name($pub_id) {
+    return str_pad($pub_id, FOLDER_DIGITS, '0', STR_PAD_LEFT);
+  }
+
   // create the output dublin core xml file for this publication
   function file_new($pub_id) {
-    if (!file_exists("import/" . $pub_id)) {
-      mkdir("import/" . $pub_id);
+    $output_dir = "import/" . $this->folder_name($pub_id);
+
+    if (!file_exists($output_dir)) {
+      mkdir($output_dir);
     }
       
-    $this->outfile = fopen("import/" . $pub_id . "/dublin_core.xml", "w");
+    $this->outfile = fopen($output_dir . "/dublin_core.xml", "w");
   }
 
   // finished writing this dublin core xml file
@@ -78,9 +101,15 @@ class RDB2DC {
    * into the individual item bundle folders
    */ 
   function move_pdf($pub_id, $filename) {
-    if (file_exists("pdfs/" . $filename)) {
-      if (DEBUG_MODE) echo "[info] Moving file " . $filename . "\n";
-      rename("pdfs/" . $filename, "import/" . $pub_id . "/" . $filename);
+    $file_esc = $this->escape_filename($filename);
+
+    if (file_exists("pdfs/" . $filename) && $filename != "") {
+      if (VERBOSE) echo "[info] Moving file " . $filename . "\n";
+
+      if (!TEST_MODE) {
+        rename("pdfs/" . $filename, "import/" . $this->folder_name($pub_id) . "/" . $filename);
+      }
+
     }
   }
 
@@ -89,10 +118,30 @@ class RDB2DC {
    * specifying which additional files belong to this item bundle
    */
   function write_contents($pub_id, $filename) {
-    $contents = fopen("import/" . $pub_id . "/contents", "w");
+    $contents = fopen("import/" . $this->folder_name($pub_id) . "/contents", "w");
     fwrite($contents, $filename . "\t" . "bundle:ORIGINAL\n");
     fclose($contents);
   }
+
+  /**
+   * escape and encode a string for an xml element
+   */
+  function clean_value($value) {
+
+    $working = trim($value);
+
+    // Removes any invalid unicode characters
+    $working = mb_convert_encoding($working, 'UTF-8', 'UTF-8');
+
+    // XML documents can't have control characters
+    $working = preg_replace('/[[:cntrl:]]/', '', $working);
+
+    // XML strings need escaped special characters
+    $filtered = htmlspecialchars($working);
+
+    return $filtered;
+  }
+
   
   /**
    * Creates a dcvalue xml tag, formatted like this:
@@ -100,6 +149,8 @@ class RDB2DC {
    * <dcvalue element="foo">bar</dcvalue>
    */
   function write_dcvalue($element, $qualifier, $value) {
+
+    // TODO: standard implementation with DOMDocument::createTextNode()?
 
     // skip on empty values
     if ($value == "" || $value == "0") return;
@@ -111,11 +162,10 @@ class RDB2DC {
       fwrite($this->outfile, " qualifier=\"" . trim($qualifier) . "\"");
     }
     fwrite($this->outfile, ">");
-	
-    // XML requires escaping <, >, &, etc.
-    $xmlvalue = htmlspecialchars(trim($value));
 
-    fwrite($this->outfile, $xmlvalue);
+    $value_xml = $this->clean_value($value);
+
+    fwrite($this->outfile, $value_xml);
     fwrite($this->outfile, "</dcvalue>\n");
   }
   
@@ -136,7 +186,10 @@ class RDB2DC {
     $sql = "select journal, volume, spage, epage, extent " .
            "from pubs_journal_new " .
            "where pub_id = ?;";
-    $this->query_pub_details($pub_id, $sql, "RDB2DC::format_journal");
+    $journals = $this->get_recordset($sql, $pub_id);
+    foreach($journals as $journal) {
+      $this->format_journal($journal);
+    }
   }
 
   function format_subject($subject) {
@@ -147,7 +200,10 @@ class RDB2DC {
     $sql = "select name " .
            "from pubs_category inner join pubs_main_cat_lnk lnk on lnk.f_category_id = pubs_category.category_id " .
            "where f_pub_id = ?";
-    $this->query_pub_details($pub_id, $sql, "RDB2DC::format_subject");
+    $subjects = $this->get_recordset($sql, $pub_id);
+    foreach($subjects as $subject) {
+      $this->format_subject($subject);
+    }
   }
 
   function format_keyword($keyword) {
@@ -158,7 +214,10 @@ class RDB2DC {
     $sql = "select name " .
            "from pubs_tag inner join pubs_main_tag_lnk lnk on lnk.f_tag_id = pubs_tag.tag_id " .
            "where f_pub_id = ?";
-    $this->query_pub_details($pub_id, $sql, "RDB2DC::format_keyword");
+    $keywords = $this->get_recordset($sql, $pub_id);
+    foreach($keywords as $keyword) {
+      $this->format_keyword($keyword);
+    }
   }
 
   function format_author($author) {
@@ -173,7 +232,11 @@ class RDB2DC {
     $sql = "select fname, lname " . 
            "from pubs_author inner join pubs_main_auth_lnk lnk on lnk.f_author_id = pubs_author.author_id " .
            "where f_pub_id = ?";
-    $this->query_pub_details($pub_id, $sql, "RDB2DC::format_author");
+
+    $authors = $this->get_recordset($sql, $pub_id);
+    foreach($authors as $author) {
+      $this->format_author($author);
+    }
   }
  
   function write_xml_header() {
@@ -194,8 +257,14 @@ class RDB2DC {
 
     $id = $pub['pub_id'];
 
-    if (DEBUG_MODE) {
+    if (VERBOSE) {
       echo("[info] " . $id . " " . $pub['title'] . "\n");
+    }
+
+    // abort if PDF is missing
+    if (!file_exists("pdfs/" . $pub['pub_file']) || $pub['pub_file'] == "") {
+      echo "[warning] Missing PDF, skipping. pub_id=" . $id . ", file=" . $pub['pub_file'] . "\n";
+      return;
     }
 
     // prepare for DSpace Batch Import Format
@@ -224,7 +293,10 @@ class RDB2DC {
    
   function query_pubs() {  
     $sql = "select pub_id, title, abstract, year, pub_file from pubs_main;";
-    $this->query_pub_details("", $sql, "RDB2DC::format_pub");
+    $publications = $this->get_recordset($sql);
+    foreach($publications as $publication) {
+      $this->format_pub($publication);
+    }
   }
   
   function export_all() {
